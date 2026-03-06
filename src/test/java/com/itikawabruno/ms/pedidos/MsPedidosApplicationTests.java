620620f416248f0c2c9617ec2c547d17fdd12af0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-06 11:26:07 -0300

    first commit

---
+++ src/test/java/com/itikawabruno/ms/pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itikawabruno.ms.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
